6e74db6f0ee87ba9af06174627607465d45beb65
skolley74 <49532933+skolley74@users.noreply.github.com>
2021-11-26 00:43:18 +0000

    Add files via upload

---
+++ person_class.py
@@ -0,0 +1,10 @@
+# This code creates a person class
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+p1 = Person("saikou", "32")
+print(p1.name)
+print(p1.age)
