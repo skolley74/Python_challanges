98d944d885a12d39ed5fc44d481fa7aae273e091
skolley74 <49532933+skolley74@users.noreply.github.com>
2021-11-30 00:30:01 +0000

    Add files via upload

---
+++ list_length.py
@@ -0,0 +1,5 @@
+#To determine how many items a list has, use the len() function:
+# Example print the number of items in the list:
+
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
---
+++ list_type.py
@@ -0,0 +1,44 @@
+# To find list data type
+
+#mylist = ["apple", "banana", "cherry"]
+#print(type(mylist))
+
+# ..........................................
+
+# You can also use the list() constructor to create a list using double round brackets:
+
+#thislist = list(("apple", "banana", "cherry"))
+#print(thislist)
+
+# ..................................................
+
+
+# List items are indexed and you can access them by referring to the index number:
+
+
+# Example print the second item of the list:
+#thislist = ["apple", "banana", "cherry"]
+#print(thislist[1])
+
+# .........................................................................
+
+# You can also specify range:
+
+# Return the third, fourth, and fifth item:
+
+#thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+#print(thislist[2:5])
+
+# ..............................................................................
+
+# To check if an item exists in a list:
+# example apple
+
+thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list")
+
+
+
+
+
